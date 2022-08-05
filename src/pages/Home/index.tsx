fda1316ba0e6268872a53a674bc6fec482548575
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-05 16:25:04 -0300

    feat(home): implement the component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+const Home = () => {
+  return (
+    <main className='homePage'>
+        <h1>Capivara Bingo</h1>
+
+        <p>Um produto de Capivara Games</p>
+    </main>
+  )
+}
+
+export default Home
