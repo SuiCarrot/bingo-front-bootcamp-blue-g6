14bcc41a01d3af2bf054ab7d4ade79da3dbb7e6a
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-08 23:25:22 -0300

    chore: base profiles + chat

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,10 +1,32 @@
-import './style.css';
+import './style.scss';
 
 const Sidebar = () => {
 
   return (
     <>
+      <body>
+      <div className="sidebar">
+        <div className='capivaraText'>Capivara</div>
+        <div>
+          <img className='img' src="http://tny.im/t0l" alt="" />
+            Perfil 1
+         </div>
+        <div>
+          <img className='img' src="http://tny.im/t0m" alt="" />
+            Perfil 2
+        </div>
 
+        <div className="chat-container">
+          <p className='olho-box'>Olho no jogo</p>
+          <p>Hello. How are you today?</p>
+        </div>  
+
+
+      </div>
+
+      <div className="body-text">
+      </div>
+      </body>
     </>
   )
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import Home from "../pages/Home";
 import { RoutesPath } from "./Paths";
+import Sidebar from "components/sidebar";
 
 const Routers = () => {
   return useRoutes([
     {
       path: RoutesPath.HOME,
-      element: <Home />,
+      //element: <Home />,
+      element: <Sidebar />,
+
     },
   ]);
 };
