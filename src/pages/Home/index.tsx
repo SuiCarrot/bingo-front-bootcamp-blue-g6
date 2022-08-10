54f02c6b9bccecb8491fc729c66ffd76cc95fcdf
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2022-08-10 15:38:22 -0300

    feat: url da imagem do avatar
    inserindo input e estilização da opção de url do avatar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,12 +8,15 @@ const Home = () => {
 
         <div className="content-box">
           <input className='nickname' type="text" placeholder="digite seu nome" />
+          <input className='nickname-image' type="text" placeholder="insira url da imagem do avatar" />
+
           <div className='game-mode'>
             <input type='radio' id='singleplayer' name='game_mode' value='singleplayer'/>
             <label>singleplayer</label>
             <input type='radio' id='multiplayer' name='game_mode' value='multiplayer'/>
             <label>multiplayer</label>
           </div>
+          
           <button>JOGAR</button>
         </div>
         
