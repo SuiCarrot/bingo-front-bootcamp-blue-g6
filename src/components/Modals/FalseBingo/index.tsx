409f2c4e9fe346bd3b03a50eb276dc9c51bd0dcd
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-11 16:52:49 -0300

    chore(FalseBingoModal): create component and implementing styles

---
+++ src/components/Modals/FalseBingo/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ContainerModal from "../ContainerModals";
+import "./style.scss";
+
+const FalseBingoModal = () => {
+  return (
+    <ContainerModal>
+        <div className="content">
+            
+        </div>
+    </ContainerModal>
+  );
+};
+
+export default FalseBingoModal;
