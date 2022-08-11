3c8ce8ee4c805c5d4dcf7a1bca78072cc5892fc9
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-11 16:52:02 -0300

    feat(GamePage): import FalseBingoModal component

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,4 +1,5 @@
 import BingoModal from 'components/Modals/BingoModal'
+import FalseBingoModal from 'components/Modals/FalseBingo'
 import GameModal from 'components/Modals/GameModal'
 import Sidebar from 'components/sidebar'
 import './style.scss'
@@ -8,7 +9,8 @@ const GamePage = () => {
     <main className='gamePage'>
         {/* <Sidebar /> */}
         {/* <GameModal /> */}
-        <BingoModal />
+        {/* <BingoModal /> */}
+        <FalseBingoModal />
     </main>
   )
 }
