ca77f48f777c323026d782e8388972b36a9d0016
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-11 22:39:52 -0300

    feat: gameBox

---
+++ src/components/gameBox/index.tsx
@@ -0,0 +1,25 @@
+import './style.scss';
+
+const GameBox = () => {
+
+  return (
+    <>
+        <div className="container-box">
+            <div className="title">
+              bola atual
+              <div className="infos">
+              <h1>anteriores</h1>
+              <h1>
+                bola
+                <h1>bing<span>o</span></h1>
+              </h1>
+              <h1>tempo</h1>
+            </div>
+            </div>
+
+        </div>
+    </>
+  )
+}
+
+export default GameBox;
--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -2,15 +2,17 @@ import BingoModal from 'components/Modals/BingoModal'
 import FalseBingoModal from 'components/Modals/FalseBingo'
 import GameModal from 'components/Modals/GameModal'
 import Sidebar from 'components/sidebar'
+import GameBox from 'components/gameBox'
 import './style.scss'
 
 const GamePage = () => {
   return (
     <main className='gamePage'>
-        {/* <Sidebar /> */}
+        <Sidebar />
+        <GameBox/>
         {/* <GameModal /> */}
         {/* <BingoModal /> */}
-        <FalseBingoModal />
+        {/* <FalseBingoModal /> */}
     </main>
   )
 }
