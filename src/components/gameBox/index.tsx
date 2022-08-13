ecd0960e14e41c22b1e469b1ab043fcaa63c5a2f
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-12 22:38:08 -0300

    chore: gameBox

--- src/components/gameBox/index.tsx
+++ src/components/gameBox/index.tsx
@@ -5,18 +5,26 @@ const GameBox = () => {
   return (
     <>
         <div className="container-box">
-            <div className="title">
-              bola atual
-              <div className="infos">
-              <h1>anteriores</h1>
-              <h1>
-                bola
-                <h1>bing<span>o</span></h1>
-              </h1>
-              <h1>tempo</h1>
-            </div>
-            </div>
-
+          <div className="title">
+            bola atual
+          </div>
+          <div className="infos">
+            <p className='anteriores-box'>
+              anteriores                
+            </p>
+            <p>
+              <div className='bola'/>
+              <p>
+                <button> bing<span>o</span>!</button>
+              </p>
+            </p>
+            <p className='tempo-box'>
+              tempo
+              <div className='tempo-nmr'>
+                8
+              </div>
+            </p>
+          </div>
         </div>
     </>
   )
