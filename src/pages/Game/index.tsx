dca9a6dd7d2a522166f39f50198b3de8c2c5da16
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-15 20:48:08 -0300

    feat:Cartela Component

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -2,6 +2,7 @@ import BingoModal from 'components/Modals/BingoModal'
 import FalseBingoModal from 'components/Modals/FalseBingo'
 import GameModal from 'components/Modals/GameModal'
 import Sidebar from 'components/sidebar'
+import Cartela from 'components/cartela'
 import './style.scss'
 
 const GamePage = () => {
@@ -10,7 +11,8 @@ const GamePage = () => {
         {/* <Sidebar /> */}
         {/* <GameModal /> */}
         {/* <BingoModal /> */}
-        <FalseBingoModal />
+        {/* <FalseBingoModal /> */}
+        <Cartela/>
     </main>
   )
 }
