2a87fae7698f3298491512b75f85c8f77091e265
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-17 23:52:47 -0300

    feat(interfaces): implement alts in interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -2,28 +2,38 @@ interface PlayerLogin {
     id?: string;
     name: string;
     avatar: string;
-    score: number;
-    isHost: boolean;
+    score?: number;
+    isHost?: boolean;
     matchId: string;
 }
 
 interface Cards {
     id?: string;
-    numbers: string[];
+    numbers: number[];
     playerId: string;
 }
 
 interface MatchGame {
+    id?: string;
     name: string;
     numberOfCards: number;
     drawTime: number;
     numberOfRounds: number;
-    link: number;
-    winner: number;
+    link?: string;
+    winner?: string;
+}
+
+interface DrawNumbers {
+    id?: string;
+    actualNumber: number;
+    lastNumbers: number[];
+    drawNumbers: number[];
+    baseNumbers: number[];
 }
 
 export type {
     PlayerLogin,
     Cards,
     MatchGame,
+    DrawNumbers,
 }
