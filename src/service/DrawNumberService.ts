caf5e0aa1e7563430e0845b2bf617feee1256ae3
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-17 23:50:24 -0300

    feat(DrawNumberService): implement connections from API

---
+++ src/service/DrawNumberService.ts
@@ -0,0 +1,51 @@
+import api from "./api";
+import { DrawNumbers } from "../types/interfaces";
+
+const route = "/draw-numbers";
+
+export const DrawNumbersService = {
+  Post: async (values: DrawNumbers) => {
+    try {
+      const res = await api.post(route, values);
+      return res;
+    } catch (error: any) {
+      console.log({ message: `Error` + error });
+    }
+  },
+
+  GetAll: async () => {
+    try {
+      const res = await api.get(route);
+      return res;
+    } catch (error: any) {
+      console.log({ message: `Error` + error });
+    }
+  },
+
+  GetById: async (id: string) => {
+    try {
+      const res = await api.get(route + `/${id}`);
+      return res;
+    } catch (error: any) {
+      console.log({ message: `Error` + error });
+    }
+  },
+
+  Patch: async (id: string, values: DrawNumbers) => {
+    try {
+      const res = await api.patch(route + `/${id}`, values);
+      return res;
+    } catch (error: any) {
+      console.log({ message: `Error` + error });
+    }
+  },
+
+  Delete: async (id: string) => {
+    try {
+      const res = await api.delete(route + `/${id}`);
+      return res;
+    } catch (error: any) {
+      console.log({ message: `Error` + error });
+    }
+  },
+};
