76b0ae4db427e2a8e3b71996c19f192aa7eadfb5
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-18 22:21:29 -0300

    Update index.tsx

--- src/components/cartela/index.tsx
+++ src/components/cartela/index.tsx
@@ -25,49 +25,49 @@ const Cartela = () => {
   return (
 
 
-        <table >
-    <thead className="card-title">
-      Cartela 01
-    </thead>
+    <table >
+      <thead className="card-title">
+        Cartela 01
+      </thead>
 
-    <tbody>
+      <tbody>
 
-      <tr>
-        <td className="tg-0lax">1</td>
-        <td className="tg-0lax">2</td>
-        <td className="tg-0lax">3</td>
-        <td className="tg-0lax">4</td>
-        <td className="tg-0lax">5</td>
-      </tr>
-      <tr>
-        <td className="tg-0lax">5</td>
-        <td className="tg-0lax">6</td>
-        <td className="tg-0lax">7</td>
-        <td className="tg-0lax">8</td>
-        <td className="tg-0lax">9</td>
-      </tr>
-      <tr>
-        <td className="tg-0lax">10</td>
-        <td className="tg-0lax">11</td>
-        <td className="free"></td>
-        <td className="tg-0lax">12</td>
-        <td className="tg-0lax">13</td>
-      </tr>
-      <tr>
-        <td className="tg-0lax">14</td>
-        <td className="tg-0lax">15</td>
-        <td className="tg-0lax">16</td>
-        <td className="tg-0lax">17</td>
-        <td className="tg-0lax">18</td>
-      </tr>
-      <tr>
-        <td className="tg-0lax">19</td>
-        <td className="tg-0lax">20</td>
-        <td className="tg-0lax">21</td>
-        <td className="tg-0lax">22</td>
-        <td className="tg-0lax">23</td>
-      </tr>
-    </tbody>
+        <tr>
+          <td className="tg-0lax">1</td>
+          <td className="tg-0lax">2</td>
+          <td className="tg-0lax">3</td>
+          <td className="tg-0lax">4</td>
+          <td className="tg-0lax">5</td>
+        </tr>
+        <tr>
+          <td className="tg-0lax">5</td>
+          <td className="tg-0lax">6</td>
+          <td className="tg-0lax">7</td>
+          <td className="tg-0lax">8</td>
+          <td className="tg-0lax">9</td>
+        </tr>
+        <tr>
+          <td className="tg-0lax">10</td>
+          <td className="tg-0lax">11</td>
+          <td className="free"></td>
+          <td className="tg-0lax">12</td>
+          <td className="tg-0lax">13</td>
+        </tr>
+        <tr>
+          <td className="tg-0lax">14</td>
+          <td className="tg-0lax">15</td>
+          <td className="tg-0lax">16</td>
+          <td className="tg-0lax">17</td>
+          <td className="tg-0lax">18</td>
+        </tr>
+        <tr>
+          <td className="tg-0lax">19</td>
+          <td className="tg-0lax">20</td>
+          <td className="tg-0lax">21</td>
+          <td className="tg-0lax">22</td>
+          <td className="tg-0lax">23</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
