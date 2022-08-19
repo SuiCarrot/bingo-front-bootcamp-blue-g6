35045d45b14d1de0166942f2ea655302a632be9e
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-18 23:57:23 -0300

    feat(Cartela): implement connection id with Card

--- src/components/cartela/index.tsx
+++ src/components/cartela/index.tsx
@@ -1,47 +1,68 @@
+import { useEffect, useState } from "react";
+import { CardService } from "service/CardService";
+import { Cards } from "types/interfaces";
 import "./style.scss";
 
 const Cartela = () => {
+  const [CardId, setCardId] = useState<Cards>();
+
+  const getCardMatch = async () => {
+    const cardId = localStorage.getItem("cardId");
+    const payloadCardId = await CardService.GetCardById(cardId);
+
+    if (payloadCardId) {
+      setCardId(payloadCardId.data);
+    } else {
+      alert("Erro ao renderizar as informações!");
+    }
+  };
+
+  useEffect(() => {
+    getCardMatch();
+  }, []);
+
   return (
-      <table>
+    <table>
+      <thead className="card-title">Cartela 01</thead>
+
+      <tbody>
         <tr>
-            <th className="card-title">Cartela 01</th>
+          <td className="tg-0lax">{CardId?.numbers[0]}</td>
+          <td className="tg-0lax">{CardId?.numbers[1]}</td>
+          <td className="tg-0lax">{CardId?.numbers[2]}</td>
+          <td className="tg-0lax">{CardId?.numbers[3]}</td>
+          <td className="tg-0lax">{CardId?.numbers[4]}</td>
         </tr>
         <tr>
-          <td className="square0">1</td>
-          <td className="square1">2</td>
-          <td className="square2">3</td>
-          <td className="square3">4</td>
-          <td className="square4">5</td>
+          <td className="tg-0lax">{CardId?.numbers[6]}</td>
+          <td className="tg-0lax">{CardId?.numbers[7]}</td>
+          <td className="tg-0lax">{CardId?.numbers[8]}</td>
+          <td className="tg-0lax">{CardId?.numbers[9]}</td>
+          <td className="tg-0lax">{CardId?.numbers[10]}</td>
         </tr>
         <tr>
-          <td className="square5">6</td>
-          <td className="square6">7</td>
-          <td className="square7">8</td>
-          <td className="square8">9</td>
-          <td className="square9">10</td>
-        </tr>
-        <tr>
-          <td className="square10">11</td>
-          <td className="square11">12</td>
+          <td className="tg-0lax">{CardId?.numbers[11]}</td>
+          <td className="tg-0lax">{CardId?.numbers[12]}</td>
           <td className="free"></td>
-          <td className="square12">13</td>
-          <td className="square13">14</td>
+          <td className="tg-0lax">{CardId?.numbers[13]}</td>
+          <td className="tg-0lax">{CardId?.numbers[14]}</td>
         </tr>
         <tr>
-          <td className="square14">15</td>
-          <td className="square15">16</td>
-          <td className="square16">17</td>
-          <td className="square17">18</td>
-          <td className="square18">19</td>
+          <td className="tg-0lax">{CardId?.numbers[15]}</td>
+          <td className="tg-0lax">{CardId?.numbers[16]}</td>
+          <td className="tg-0lax">{CardId?.numbers[17]}</td>
+          <td className="tg-0lax">{CardId?.numbers[18]}</td>
+          <td className="tg-0lax">{CardId?.numbers[19]}</td>
         </tr>
         <tr>
-          <td className="square19">20</td>
-          <td className="square20">21</td>
-          <td className="square21">22</td>
-          <td className="square22">23</td>
-          <td className="square23">24</td>
+          <td className="tg-0lax">{CardId?.numbers[20]}</td>
+          <td className="tg-0lax">{CardId?.numbers[21]}</td>
+          <td className="tg-0lax">{CardId?.numbers[22]}</td>
+          <td className="tg-0lax">{CardId?.numbers[23]}</td>
+          <td className="tg-0lax">{CardId?.numbers[24]}</td>
         </tr>
-      </table>
+      </tbody>
+    </table>
   );
 };
 
