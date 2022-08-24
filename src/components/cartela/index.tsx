7a43ecf40e3ed8659d1082a5da21972ca6e8dc37
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-08-24 00:34:26 -0300

    chore(checkCard): change Color When Clicked

--- src/components/cartela/index.tsx
+++ src/components/cartela/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import { CardService } from "service/CardService";
 import { Cards } from "types/interfaces";
 import "./style.scss";
@@ -21,45 +21,51 @@ const Cartela = () => {
     getCardMatch();
   }, []);
 
+  const [changeColor, setchangeColor] = useState(false);
+
+  const toggle = ()=>{
+    setchangeColor(!changeColor)
+  }
+
   return (
     <table>
       <thead className="card-title">Cartela 01</thead>
 
       <tbody>
         <tr>
-          <td className="tg-0lax">{CardId?.numbers[0]}</td>
-          <td className="tg-0lax">{CardId?.numbers[1]}</td>
-          <td className="tg-0lax">{CardId?.numbers[2]}</td>
-          <td className="tg-0lax">{CardId?.numbers[3]}</td>
-          <td className="tg-0lax">{CardId?.numbers[4]}</td>
+          <td onClick={toggle} className={(changeColor ? 'toggle--close' : '')} >{CardId?.numbers[0]}</td>
+          <td onClick={toggle} className={(changeColor ? 'toggle--close' : '')} >{CardId?.numbers[1]}</td>
+          <td className="numberBox">{CardId?.numbers[2]}</td>
+          <td className="numberBox">{CardId?.numbers[3]}</td>
+          <td className="numberBox">{CardId?.numbers[4]}</td>
         </tr>
         <tr>
-          <td className="tg-0lax">{CardId?.numbers[6]}</td>
-          <td className="tg-0lax">{CardId?.numbers[7]}</td>
-          <td className="tg-0lax">{CardId?.numbers[8]}</td>
-          <td className="tg-0lax">{CardId?.numbers[9]}</td>
-          <td className="tg-0lax">{CardId?.numbers[10]}</td>
+          <td className="numberBox">{CardId?.numbers[6]}</td>
+          <td className="numberBox">{CardId?.numbers[7]}</td>
+          <td className="numberBox">{CardId?.numbers[8]}</td>
+          <td className="numberBox">{CardId?.numbers[9]}</td>
+          <td className="numberBox">{CardId?.numbers[10]}</td>
         </tr>
         <tr>
-          <td className="tg-0lax">{CardId?.numbers[11]}</td>
-          <td className="tg-0lax">{CardId?.numbers[12]}</td>
+          <td className="numberBox">{CardId?.numbers[11]}</td>
+          <td className="numberBox">{CardId?.numbers[12]}</td>
           <td className="free"></td>
-          <td className="tg-0lax">{CardId?.numbers[13]}</td>
-          <td className="tg-0lax">{CardId?.numbers[14]}</td>
+          <td className="numberBox">{CardId?.numbers[13]}</td>
+          <td className="numberBox">{CardId?.numbers[14]}</td>
         </tr>
         <tr>
-          <td className="tg-0lax">{CardId?.numbers[15]}</td>
-          <td className="tg-0lax">{CardId?.numbers[16]}</td>
-          <td className="tg-0lax">{CardId?.numbers[17]}</td>
-          <td className="tg-0lax">{CardId?.numbers[18]}</td>
-          <td className="tg-0lax">{CardId?.numbers[19]}</td>
+          <td className="numberBox">{CardId?.numbers[15]}</td>
+          <td className="numberBox">{CardId?.numbers[16]}</td>
+          <td className="numberBox">{CardId?.numbers[17]}</td>
+          <td className="numberBox">{CardId?.numbers[18]}</td>
+          <td className="numberBox">{CardId?.numbers[19]}</td>
         </tr>
         <tr>
-          <td className="tg-0lax">{CardId?.numbers[20]}</td>
-          <td className="tg-0lax">{CardId?.numbers[21]}</td>
-          <td className="tg-0lax">{CardId?.numbers[22]}</td>
-          <td className="tg-0lax">{CardId?.numbers[23]}</td>
-          <td className="tg-0lax">{CardId?.numbers[24]}</td>
+          <td className="numberBox">{CardId?.numbers[20]}</td>
+          <td className="numberBox">{CardId?.numbers[21]}</td>
+          <td className="numberBox">{CardId?.numbers[22]}</td>
+          <td className="numberBox">{CardId?.numbers[23]}</td>
+          <td className="numberBox">{CardId?.numbers[24]}</td>
         </tr>
       </tbody>
     </table>
