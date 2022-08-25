600da447bb14d58fb837e91857262e440f796740
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-25 19:12:07 -0300

    feat(GamePage): implement logics

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,28 +1,58 @@
-import BingoModal from 'components/Modals/BingoModal'
-import FalseBingoModal from 'components/Modals/FalseBingo'
-import GameModal from 'components/Modals/GameModal'
-import LoserModal from 'components/Modals/LoserModal'
-import Sidebar from 'components/sidebar'
-import Cartela from 'components/cartela'
-import GameBox from 'components/gameBox'
-
-import './style.scss'
-import PlayerModal from 'components/Modals/PlayerModal'
-import { useState } from 'react'
+import BingoModal from "components/Modals/BingoModal";
+import FalseBingoModal from "components/Modals/FalseBingo";
+import GameModal from "components/Modals/GameModal";
+import LoserModal from "components/Modals/LoserModal";
+import Sidebar from "components/sidebar";
+import Cartela from "components/cartela";
+import GameBox from "components/gameBox";
+import "./style.scss";
+import PlayerModal from "components/Modals/PlayerModal";
+import { useEffect, useState } from "react";
 
 const GamePage = () => {
-  return (
-    <main className='gamePage'>
-        {/* <Sidebar /> */}
-        {/* <FalseBingoModal /> */}
-        {/* <Cartela/> */}
-        <GameBox/>
-        {/* <GameModal /> */}
-        {/* <BingoModal /> */}
-        {/* <LoserModal /> */}
-        {/* <FalseBingoModal /> */}
-    </main>
-  )
-}
+  const resultBingo = localStorage.getItem("bingoResult");
 
-export default GamePage
+  const [bingoResult, setBingoResult] = useState<boolean>();
+
+  const [bingoModal, setBingoModal] = useState(false);
+  const [falseBingoModal, setFalseBingoModal] = useState(false);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const showAModal = () => {
+    if (resultBingo === "false") {
+      setFalseBingoModal(!falseBingoModal);
+      setShowModal(true);
+    } else if (resultBingo === "true") {
+      setBingoModal(!bingoModal);
+      setShowModal(true);
+    }
+  };
+
+  useEffect(() => {
+    showAModal();
+  }, []);
+
+  return (
+    <main className="gamePage">
+      {/* <Sidebar /> */}
+      {/* <FalseBingoModal /> */}
+      {/* <Cartela/> */}
+
+      <GameBox />
+
+      {bingoModal && showModal && <BingoModal />}
+
+      {falseBingoModal && showModal && (
+        <FalseBingoModal closeModal={setShowModal} />
+      )}
+
+      {/* <BingoModal /> */}
+      {/* <FalseBingoModal /> */}
+      {/* <GameModal /> */}
+      {/* <LoserModal /> */}
+    </main>
+  );
+};
+
+export default GamePage;
