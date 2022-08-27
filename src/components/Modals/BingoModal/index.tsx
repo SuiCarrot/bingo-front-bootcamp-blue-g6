a3da9b3c968e50a78c7c7c1881dff57978e967f3
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-26 21:32:09 -0300

    feat(BingoModal): implement closeModal function and style prettier

--- src/components/Modals/BingoModal/index.tsx
+++ src/components/Modals/BingoModal/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import ContainerModal from '../ContainerModals';
-import './style.scss';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ContainerModal from "../ContainerModals";
+import "./style.scss";
 
-const BingoModal = () => {
-  return (
-    <ContainerModal>
-        <div className='bingo-content'>
-          <h2>bing<span>o</span>!</h2>
-
-          <h3>Parabéns!!!</h3>
-
-          <p>Você é o vencedor dessa rodada! merece o selo capivara</p>
-        </div>
-    </ContainerModal>
-  )
+interface Prop {
+  closeModal: (value: any) => void;
 }
 
-export default BingoModal
+const BingoModal = ({ closeModal }: Prop) => {
+  const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    closeModal(false);
+    navigate("/");
+  };
+
+  return (
+    <ContainerModal>
+      <div className="bingo-content" onClick={handleCloseModal}>
+        <h2>
+          bing<span>o</span>!
+        </h2>
+
+        <h3>Parabéns!!!</h3>
+
+        <p>Você é o vencedor dessa rodada! merece o selo capivara</p>
+      </div>
+    </ContainerModal>
+  );
+};
+
+export default BingoModal;
