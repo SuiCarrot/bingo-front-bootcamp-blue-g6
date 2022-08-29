be35e6ddeec337d34bc4e51c2e71089e5aad46a5
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-29 20:08:46 -0300

    feat(Cartela): implement context, remove getCardMatch and implement map with state context

--- src/components/cartela/index.tsx
+++ src/components/cartela/index.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
-import { CardService } from "service/CardService";
-import { Cards } from "types/interfaces";
+import { useCards } from "context/CardsContext";
+import { useState } from "react";
+import { CardsContextType } from "types/interfaces";
 import "./style.scss";
 
 const Cartela = () => {
+  const { quantityCards } = useCards() as CardsContextType;
+
   const [changeColorOne, setchangeColorOne] = useState(false);
   const [changeColorTwo, setchangeColorTwo] = useState(false);
   const [changeColorThree, setchangeColorThree] = useState(false);
@@ -116,185 +118,172 @@ const Cartela = () => {
     setchangeColorTwentyfour(!changeColorTwentyfour);
   };
 
-  const [CardId, setCardId] = useState<Cards>();
-
-  const getCardMatch = async () => {
-    const cardId = localStorage.getItem("cardId");
-    const payloadCardId = await CardService.GetCardById(cardId);
-
-    if (payloadCardId) {
-      setCardId(payloadCardId.data);
-    } else {
-      alert("Erro ao renderizar as informações!");
-    }
-  };
-
-  useEffect(() => {
-    getCardMatch();
-  }, []);
-
   return (
-    <table>
-      <thead className="card-title">Cartela</thead>
+    <>
+      {quantityCards.map((card, index) => (
+        <table key={index}>
+          <thead className="card-title">Cartela {index + 1}</thead>
 
-      <tbody>
-        <tr>
-          <td
-            onClick={toggleOne}
-            className={changeColorOne ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[0]}
-          </td>
-          <td
-            onClick={toggleTwo}
-            className={changeColorTwo ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[1]}
-          </td>
-          <td
-            onClick={toggleThree}
-            className={changeColorThree ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[2]}
-          </td>
-          <td
-            onClick={togglefour}
-            className={changeColorFour ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[3]}
-          </td>
-          <td
-            onClick={togglefive}
-            className={changeColorFive ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[4]}
-          </td>
-        </tr>
-        <tr>
-          <td
-            onClick={toggleSix}
-            className={changeColorSix ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[6]}
-          </td>
-          <td
-            onClick={toggleseven}
-            className={changeColorSeven ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[7]}
-          </td>
-          <td
-            onClick={toggleeight}
-            className={changeColorEight ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[8]}
-          </td>
-          <td
-            onClick={togglenine}
-            className={changeColorNine ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[9]}
-          </td>
-          <td
-            onClick={toggleten}
-            className={changeColorTen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[10]}
-          </td>
-        </tr>
-        <tr>
-          <td
-            onClick={toggleeleven}
-            className={changeColorEleven ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[11]}
-          </td>
-          <td
-            onClick={toggletwelve}
-            className={changeColorTwelve ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[12]}
-          </td>
-          <td className="free"></td>
-          <td
-            onClick={togglethirteen}
-            className={changeColorThirteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[13]}
-          </td>
-          <td
-            onClick={togglefourteen}
-            className={changeColorFourteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[14]}
-          </td>
-        </tr>
-        <tr>
-          <td
-            onClick={togglefifteen}
-            className={changeColorFifteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[15]}
-          </td>
-          <td
-            onClick={togglesixteen}
-            className={changeColorSixteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[16]}
-          </td>
-          <td
-            onClick={toggleseventeen}
-            className={changeColorSeventeen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[17]}
-          </td>
-          <td
-            onClick={toggleeighteen}
-            className={changeColorEighteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[18]}
-          </td>
-          <td
-            onClick={togglenineteen}
-            className={changeColorNineteen ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[19]}
-          </td>
-        </tr>
-        <tr>
-          <td
-            onClick={toggletwenty}
-            className={changeColorTwenty ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[20]}
-          </td>
-          <td
-            onClick={toggletwentyone}
-            className={changeColorTwentyone ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[21]}
-          </td>
-          <td
-            onClick={toggletwentytwo}
-            className={changeColorTwentytwo ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[22]}
-          </td>
-          <td
-            onClick={toggletwentythree}
-            className={changeColorTwentythree ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[23]}
-          </td>
-          <td
-            onClick={toggletwentyfour}
-            className={changeColorTwentyfour ? "toggle--close" : ""}
-          >
-            {CardId?.numbers[24]}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          <tbody>
+            <tr>
+              <td
+                onClick={toggleOne}
+                className={changeColorOne ? "toggle--close" : ""}
+              >
+                {card.numbers[0]}
+              </td>
+              <td
+                onClick={toggleTwo}
+                className={changeColorTwo ? "toggle--close" : ""}
+              >
+                {card.numbers[1]}
+              </td>
+              <td
+                onClick={toggleThree}
+                className={changeColorThree ? "toggle--close" : ""}
+              >
+                {card.numbers[2]}
+              </td>
+              <td
+                onClick={togglefour}
+                className={changeColorFour ? "toggle--close" : ""}
+              >
+                {card.numbers[3]}
+              </td>
+              <td
+                onClick={togglefive}
+                className={changeColorFive ? "toggle--close" : ""}
+              >
+                {card.numbers[4]}
+              </td>
+            </tr>
+            <tr>
+              <td
+                onClick={toggleSix}
+                className={changeColorSix ? "toggle--close" : ""}
+              >
+                {card.numbers[6]}
+              </td>
+              <td
+                onClick={toggleseven}
+                className={changeColorSeven ? "toggle--close" : ""}
+              >
+                {card.numbers[7]}
+              </td>
+              <td
+                onClick={toggleeight}
+                className={changeColorEight ? "toggle--close" : ""}
+              >
+                {card.numbers[8]}
+              </td>
+              <td
+                onClick={togglenine}
+                className={changeColorNine ? "toggle--close" : ""}
+              >
+                {card.numbers[9]}
+              </td>
+              <td
+                onClick={toggleten}
+                className={changeColorTen ? "toggle--close" : ""}
+              >
+                {card.numbers[10]}
+              </td>
+            </tr>
+            <tr>
+              <td
+                onClick={toggleeleven}
+                className={changeColorEleven ? "toggle--close" : ""}
+              >
+                {card.numbers[11]}
+              </td>
+              <td
+                onClick={toggletwelve}
+                className={changeColorTwelve ? "toggle--close" : ""}
+              >
+                {card.numbers[12]}
+              </td>
+              <td className="free"></td>
+              <td
+                onClick={togglethirteen}
+                className={changeColorThirteen ? "toggle--close" : ""}
+              >
+                {card.numbers[13]}
+              </td>
+              <td
+                onClick={togglefourteen}
+                className={changeColorFourteen ? "toggle--close" : ""}
+              >
+                {card.numbers[14]}
+              </td>
+            </tr>
+            <tr>
+              <td
+                onClick={togglefifteen}
+                className={changeColorFifteen ? "toggle--close" : ""}
+              >
+                {card.numbers[15]}
+              </td>
+              <td
+                onClick={togglesixteen}
+                className={changeColorSixteen ? "toggle--close" : ""}
+              >
+                {card.numbers[16]}
+              </td>
+              <td
+                onClick={toggleseventeen}
+                className={changeColorSeventeen ? "toggle--close" : ""}
+              >
+                {card.numbers[17]}
+              </td>
+              <td
+                onClick={toggleeighteen}
+                className={changeColorEighteen ? "toggle--close" : ""}
+              >
+                {card.numbers[18]}
+              </td>
+              <td
+                onClick={togglenineteen}
+                className={changeColorNineteen ? "toggle--close" : ""}
+              >
+                {card.numbers[19]}
+              </td>
+            </tr>
+            <tr>
+              <td
+                onClick={toggletwenty}
+                className={changeColorTwenty ? "toggle--close" : ""}
+              >
+                {card.numbers[20]}
+              </td>
+              <td
+                onClick={toggletwentyone}
+                className={changeColorTwentyone ? "toggle--close" : ""}
+              >
+                {card.numbers[21]}
+              </td>
+              <td
+                onClick={toggletwentytwo}
+                className={changeColorTwentytwo ? "toggle--close" : ""}
+              >
+                {card.numbers[22]}
+              </td>
+              <td
+                onClick={toggletwentythree}
+                className={changeColorTwentythree ? "toggle--close" : ""}
+              >
+                {card.numbers[23]}
+              </td>
+              <td
+                onClick={toggletwentyfour}
+                className={changeColorTwentyfour ? "toggle--close" : ""}
+              >
+                {card.numbers[24]}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ))}
+    </>
   );
 };
 
