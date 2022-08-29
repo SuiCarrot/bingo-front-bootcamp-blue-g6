8550318d9d044aadf4fcc6bdfc6de7ffc82ab4b7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-29 20:06:53 -0300

    chore(Api): connection with deploy

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://capivara-bingo.onrender.com'
 })
 
 // http://localhost:3333 -- localhost
