43ae0ba7d0eded19b7c538ec4e320cec5f1dbe22
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-31 19:47:14 -0300

    style(CartelaCard): formated prettier

--- src/components/cartelas/index.tsx
+++ src/components/cartelas/index.tsx
@@ -1,188 +1,107 @@
 type CartelaCard = {
+  numbers: number[];
+  index: number;
+};
 
-    numbers:number[];
-    index:number;
+export function CardCartela(card: CartelaCard) {
+  const handleClick: any = (e: any) => {
+    if (e.currentTarget.style.backgroundColor) {
+      e.currentTarget.style.backgroundColor = null;
+    } else {
+      e.currentTarget.style.backgroundColor = "green";
+    }
+    e.currentTarget.classList.add("mark", "mark1");
+  };
 
+  return (
+    <table key={card.index}>
+      <thead className="card-title">Cartela {card.index + 1}</thead>
+
+      <tbody>
+        <tr>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[0]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[1]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[2]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[3]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[4]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[6]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[7]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[8]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[9]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[10]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[11]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[12]}
+          </td>
+          <td className="free"></td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[13]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[14]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[15]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[16]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[17]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[18]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[19]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[20]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[21]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[22]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[23]}
+          </td>
+          <td onClick={handleClick} className="mark">
+            {card.numbers[24]}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
-
-
-export function CardCartela(card: CartelaCard){
-
-    const handleClick:any = (e:any) => {
-        if(e.currentTarget.style.backgroundColor){
-          e.currentTarget.style.backgroundColor = null;
-        }else{
-          e.currentTarget.style.backgroundColor='green'
-        }
-        e.currentTarget.classList.add('mark', 'mark1')
-      }
-      
-
-
-    
-    return (
-
-        <table key={card.index}>
-          <thead className="card-title">Cartela {card.index + 1}</thead>
-
-          <tbody>
-            <tr>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[0]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[1]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[2]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[3]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[4]}
-              </td>
-            </tr>
-            <tr>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[6]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[7]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[8]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[9]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[10]}
-              </td>
-            </tr>
-            <tr>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[11]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[12]}
-              </td>
-              <td className="free"></td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[13]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[14]}
-              </td>
-            </tr>
-            <tr>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[15]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[16]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[17]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[18]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[19]}
-              </td>
-            </tr>
-            <tr>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[20]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[21]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[22]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[23]}
-              </td>
-              <td
-                onClick={handleClick}
-                className="mark"
-              >
-                {card.numbers[24]}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-    )
-}
-
