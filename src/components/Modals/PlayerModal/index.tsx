f2238bdba18a13aec085f56fa3b70b9eff6570ba
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-31 22:44:05 -0300

    feat(PlayerModal): usePlayer context, remove state and implement main-playerContent

--- src/components/Modals/PlayerModal/index.tsx
+++ src/components/Modals/PlayerModal/index.tsx
@@ -1,13 +1,14 @@
 import { useCards } from "context/CardsContext";
 import { useMatch } from "context/matchContext";
-import React, { useState } from "react";
+import { usePlayer } from "context/PlayerContext";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CardService } from "service/CardService";
 import { Login } from "service/LoginPlayer";
 import {
   CardsContextType,
   MatchGameContextType,
-  PlayerLogin
+  PlayerContext,
 } from "types/interfaces";
 import "./style.scss";
 
@@ -19,19 +20,14 @@ const PlayerModal = () => {
 
   const { setQuantityCards } = useCards() as CardsContextType;
 
-  const [valuesLogin, setValuesLogin] = useState<PlayerLogin>({
-    name: "",
-    avatar: "",
-    score: 0,
-    isHost: false,
-    matchId: id,
-  });
+  const { valuesLogin, setValuesLogin } = usePlayer() as PlayerContext;
 
   const handleValuesPlayer = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValuesLogin((values: PlayerLogin) => ({
-      ...values,
+    setValuesLogin({
+      ...valuesLogin,
+      matchId: id,
       [e.target.name]: e.target.value,
-    }));
+    });
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -56,7 +52,7 @@ const PlayerModal = () => {
 
     if (payloadCard) {
       setQuantityCards(payloadCard.data);
-      localStorage.setItem('cardId', payloadCard.data[0].id);
+      localStorage.setItem("cardId", payloadCard.data[0].id);
       navigate("/capybaraGame");
     } else {
       alert("Erro ao gerar os numeros do(s) card(s)!");
@@ -64,34 +60,36 @@ const PlayerModal = () => {
   };
 
   return (
-    <section className="container-playerModal">
-      <div className="content-player">
-        <h2>Cadastrar Player</h2>
+    <main className="main-playerContent">
+      <section className="container-playerModal">
+        <div className="content-player">
+          <h2>Cadastrar Player</h2>
 
-        <form onSubmit={handleSubmit} className="submit-player">
-          <input
-            autoComplete="off"
-            type="text"
-            placeholder="digite seu nome"
-            name="name"
-            id="name"
-            onChange={handleValuesPlayer}
-            required
-          />
+          <form onSubmit={handleSubmit} className="submit-player">
+            <input
+              autoComplete="off"
+              type="text"
+              placeholder="digite seu nome"
+              name="name"
+              id="name"
+              onChange={handleValuesPlayer}
+              required
+            />
 
-          <input
-            type="text"
-            placeholder="Coloque uma url de avatar"
-            name="avatar"
-            id="avatar"
-            onChange={handleValuesPlayer}
-            required
-          />
+            <input
+              type="text"
+              placeholder="Coloque uma url de avatar"
+              name="avatar"
+              id="avatar"
+              onChange={handleValuesPlayer}
+              required
+            />
 
-          <button type="submit">Logar</button>
-        </form>
-      </div>
-    </section>
+            <button type="submit">Logar</button>
+          </form>
+        </div>
+      </section>
+    </main>
   );
 };
 
